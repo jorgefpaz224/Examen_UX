1abf606ce3457a8b9052f50bf9edf14161eeaa60
jorgefpaz224 <jorgefpaz224@gmail.com>
2024-12-11 10:47:39 -0600

    Agregado componente de las cartas de restaurantes

--- frontend/src/components/PageRestaurantes.jsx
+++ frontend/src/components/PageRestaurantes.jsx
@@ -5,11 +5,17 @@ import Restaurant from './Restaurant';
 import RestaurantCard from './RestaurantCard';
 
 function PageRestaurantes() {
+  const [restaurants, setRestaurants] = useState([
+    { id: 1, name: 'Restaurant A', description: 'Description A' },
+    { id: 2, name: 'Restaurant B', description: 'Description B' },
+    { id: 3, name: 'Restaurant C', description: 'Description C' }
+  ]);
 
   return (
-  <div>
-    <Restaurant />
-  </div>
+    <div>
+      <h1> BIENVENIDO A NUESTROS RESTAURANTES</h1>
+      <Restaurant />
+    </div>
   )
 }
 
--- frontend/src/components/RestaurantCard.jsx
+++ frontend/src/components/RestaurantCard.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import Card from 'react-bootstrap/Card'
+import Button from 'react-bootstrap/Button'
 
-function RestaurantCard() {
+function RestaurantCard(nombre_restaurante, descripcion_restaurante) {
   return (
     <div>
-      
+      <Card style={{ width: '18rem' }}>
+        <Card.Img variant="top" src="https://via.placeholder.com/150" />
+        <Card.Body>
+          <Card.Title>EL NOMBRE DEL RESTAURANTE</Card.Title>
+          <Card.Text>
+            Aqui iria la descripcion
+          </Card.Text>
+          <Button variant="primary">Go somewhere</Button>
+        </Card.Body>
+      </Card>
     </div>
   )
 }
